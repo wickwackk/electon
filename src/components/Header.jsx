db353796816aa0e2d3678ebc5c315288e70b25d4
Morning <>
2023-01-13 12:30:24 +0800

    jan13 at class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-asdf;
+export default function Header() {
+  return (
+    <div>
+      <img src="./electonLogo.png" alt="" />
+      <form action="">
+        <input type="search" placeholder="Search anything" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
